9a25002dcf6628d05c2d44a0193e91167dd0b561
Joel Jinkinson <joel.jinkinson@gmail.com>
2022-07-15 20:43:23 +0100

    Started level editor

---
+++ GMTK2022/Assets/LevelGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    void Start()
+    {
+        GenerateLevel();
+           
+    }
+    void GenerateLevel() {
+        for (int x=0; x<map.width;x++) {
+            for (int y=0;y<map.height;y++) {
+                GenerateTile(x, y);
+            }
+        }
+    }
+    void GenerateTile(int x, int y) {
+        Color pixelColor = map.GetPixel(x, y);
+        foreach(ColorToPrefab colorMapping in colorMappings){
+            if (colorMapping.color.Equals(pixelColor)) {
+                Vector2 position = new Vector2(x,y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+            Debug.Log(colorMapping.color);
+        }
+    }
+}
