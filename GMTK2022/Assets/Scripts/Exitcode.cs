8fce9d3737c72e9958a91af37e55e79331606207
AceCalamari <109314301+AceCalamari@users.noreply.github.com>
2022-07-15 23:46:15 +0100

    ui change
    makes exit button work.
    msg pops up in debug console because exit code won't do anything until the game is exported as a .exe/other file type file.

---
+++ GMTK2022/Assets/Scripts/Exitcode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitcode : MonoBehaviour
+{
+    public void QuitGame () {
+ Application.Quit ();
+ Debug.Log("Game is exiting");
+ //Just to make sure its working
+ }
+ }
+
