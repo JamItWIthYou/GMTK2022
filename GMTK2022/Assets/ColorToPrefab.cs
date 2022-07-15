b95c6eeabd42bf3b5e271bad15f93ebce01f2f6e
ArgonCat <60944462+ArgonCat@users.noreply.github.com>
2022-07-15 21:28:37 +0100

    Added collision array

--- GMTK2022/Assets/ColorToPrefab.cs
+++ GMTK2022/Assets/ColorToPrefab.cs
@@ -3,8 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class ColorToPrefab
+public class Tile
 {
    public Color color;
    public GameObject prefab;
+   public CollisionType collisionType;
+}
+
+public enum CollisionType {
+   Full,
+   Half,
+   None
 }
--- GMTK2022/Assets/LevelGenerator.cs
+++ GMTK2022/Assets/LevelGenerator.cs
@@ -6,31 +6,34 @@ public class LevelGenerator : MonoBehaviour
 {
 
     public Texture2D map;
-
-    public ColorToPrefab[] colorMappings;
+    public Tile[] colorMappings;
+    public CollisionType[,] tileCollidables;
 
     void Start()
     {
         GenerateLevel();
     }
+
     void GenerateLevel() {
+        
         for (int x=0; x<map.width;x++) {
+            tileCollidables = new CollisionType[map.width, map.height];
             for (int y=0; y<map.height ;y++) {
                 GenerateTile(x, y);
             }
         }
     }
+
     void GenerateTile(int x, int y) {
         Color pixelColor = map.GetPixel(x, y);
-        Debug.Log(pixelColor);
-        foreach(ColorToPrefab colorMapping in colorMappings){
+        foreach(Tile colorMapping in colorMappings){
             if (colorMapping.color.Equals(pixelColor)) {
                 Vector2 position = new Vector2(x,y);
                 Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
-                //Debug.Log(colorMapping.color);
-                
+                tileCollidables[x, y] = colorMapping.collisionType;
+            } else {
+                tileCollidables[x, y] = CollisionType.None;
             }
-            
         }
     }
 }
