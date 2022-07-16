e4251fde001b039bc7b383390429b33fbbcc6b77
ArgonCat <60944462+ArgonCat@users.noreply.github.com>
2022-07-16 13:25:22 +0100

    Movement Funtional

--- GMTK2022/Assets/Scripts/Movement.cs
+++ GMTK2022/Assets/Scripts/Movement.cs
@@ -5,33 +5,25 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public LevelGenerator levelGenerator;
-
-    public bool isOnHalfBlock;
     public int x;
     public int y;
-
-    void Start() {
-        
-    }
 
     void Update() {
         if (y-1 < levelGenerator.tileCollidables.GetLength(1)) {
             if (levelGenerator.tileCollidables[x, y-1]==CollisionType.None) { y--; };
         }
         if(Input.GetKeyDown("a")) {
-            if (levelGenerator.tileCollidables[x-1, y]==CollisionType.None) {
-                if (levelGenerator.tileCollidables[x-1, y-1]==CollisionType.Half) {
-                    isOnHalfBlock = true;
-                } else {
-                    isOnHalfBlock = false;
-                }
-                x--;
-            } else if (levelGenerator.tileCollidables[x-1, y]==CollisionType.Half) {
-                isOnHalfBlock = true;
-                x--;
-                y++;
+            if (levelGenerator.tileCollidables[x-1, y]==CollisionType.None) {x--;}
+            else if (levelGenerator.tileCollidables[x-1, y]==CollisionType.Full) {
+                if(levelGenerator.tileCollidables[x-1, y+1]==CollisionType.None) {x--;y++;}
             }
         }
-        transform.position = new Vector3(x, y + (isOnHalfBlock?(-0.5f):0), 0);
+        if(Input.GetKeyDown("d")) {
+            if (levelGenerator.tileCollidables[x+1, y]==CollisionType.None) {x++;}
+            else if (levelGenerator.tileCollidables[x+1, y]==CollisionType.Full) {
+                if(levelGenerator.tileCollidables[x+1, y+1]==CollisionType.None) {x++;y++;}
+            }
+        }
+        transform.position = new Vector3(x, y, 0);
     }
 }
