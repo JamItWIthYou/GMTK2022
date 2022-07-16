83f4e60a3753122548b68c3998399d0400c83138
ArgonCat <60944462+ArgonCat@users.noreply.github.com>
2022-07-16 13:34:09 +0100

    Added move counter

--- GMTK2022/Assets/Scripts/Movement.cs
+++ GMTK2022/Assets/Scripts/Movement.cs
@@ -8,22 +8,34 @@ public class Movement : MonoBehaviour
     public int x;
     public int y;
 
+    public int movesRemaining;
+
     void Update() {
-        if (y-1 < levelGenerator.tileCollidables.GetLength(1)) {
-            if (levelGenerator.tileCollidables[x, y-1]==CollisionType.None) { y--; };
+        if (movesRemaining>0) {
+            Controls();
         }
-        if(Input.GetKeyDown("a")) {
-            if (levelGenerator.tileCollidables[x-1, y]==CollisionType.None) {x--;}
-            else if (levelGenerator.tileCollidables[x-1, y]==CollisionType.Full) {
-                if(levelGenerator.tileCollidables[x-1, y+1]==CollisionType.None) {x--;y++;}
-            }
-        }
-        if(Input.GetKeyDown("d")) {
-            if (levelGenerator.tileCollidables[x+1, y]==CollisionType.None) {x++;}
-            else if (levelGenerator.tileCollidables[x+1, y]==CollisionType.Full) {
-                if(levelGenerator.tileCollidables[x+1, y+1]==CollisionType.None) {x++;y++;}
-            }
-        }
+        Gravity();
         transform.position = new Vector3(x, y, 0);
+    }
+
+    void Controls () {
+        if(Input.GetKeyDown("a")) {
+            if (levelGenerator.tileCollidables[x-1, y]==CollisionType.None) {x--;movesRemaining--;}
+            else if (levelGenerator.tileCollidables[x-1, y]==CollisionType.Full) {
+                if(levelGenerator.tileCollidables[x-1, y+1]==CollisionType.None) {x--;y++;movesRemaining--;}
+            }
+        }
+        else if(Input.GetKeyDown("d")) {
+            if (levelGenerator.tileCollidables[x+1, y]==CollisionType.None) {x++;movesRemaining--;}
+            else if (levelGenerator.tileCollidables[x+1, y]==CollisionType.Full) {
+                if(levelGenerator.tileCollidables[x+1, y+1]==CollisionType.None) {x++;y++;movesRemaining--;}
+            }
+        }
+    }
+
+    void Gravity() {
+        if (y-1 < levelGenerator.tileCollidables.GetLength(1)) {
+            if (levelGenerator.tileCollidables[x, y-1]==CollisionType.None) {y--;};
+        }
     }
 }
